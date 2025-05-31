b8e965ea2b37716167a39b0f9557e10a99a6474b
이관현\Luganic <rhksgus509@gmail.com>
2025-06-01 00:36:08 +0900

    #13 내 크레딧 수량 자리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import HeaderNav from "./component/10_HeaderNav/HeaderNav";
+import MyCredit from "./component/13_MyCredit/MyCredit"; //임시
 // import testImage from "./assets/image/test_image2.png"
 
 const App = () => {
   return (
     <>
       <HeaderNav />
-      <h1>메인 라우터</h1>
-      <p>라우터 작성전까지 테스트</p>
+      <MyCredit />
     </>
   );
 };
