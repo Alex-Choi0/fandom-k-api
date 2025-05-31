1acfc69c2dfbc7416e8769d4f23cb809e719f0f3
이관현\Luganic <rhksgus509@gmail.com>
2025-06-01 00:54:11 +0900

    #17 충전하기 버튼 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import HeaderNav from "./component/10_HeaderNav/HeaderNav";
 import MyCredit from "./component/13_MyCredit/MyCredit"; //임시
+import CreditCharge from "./component/17_CreditCharge/CreditCharge"; //임시
 // import testImage from "./assets/image/test_image2.png"
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <HeaderNav />
       <MyCredit />
+      <CreditCharge />
     </>
   );
 };
