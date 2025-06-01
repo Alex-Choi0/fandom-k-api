8a51628d7e59a6ac6826415cd02011c673e5824b
alex <alex@gmail.com>
2025-06-01 14:36:34 +0900

    #20 아이돌의 차트(랭킹)을 불러온다.

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -12,19 +12,19 @@ const mainUrl = 'https://fandom-k-api.vercel.app'
 export const FindIdols19 = async (teamName = '뉴진스', cursor = null, pageSize, keyword = null) => {
 
   const arr = [{ cursor }, { pageSize }, { keyword }]
-  let str = '';
+  let str = createQuery(arr)
 
-  for (let i = 0; i < arr.length; i++) {
-    const key = Object.keys(arr[i])[0]
-    console.log("check key : ", key);
+  // for (let i = 0; i < arr.length; i++) {
+  //   const key = Object.keys(arr[i])[0]
+  //   console.log("check key : ", key);
 
-    // null이 아닌 값은 쿼리로 입력
-    if (arr[i][key]) {
-      str += `${key}=${arr[i][key]}`
-    }
+  //   // null이 아닌 값은 쿼리로 입력
+  //   if (arr[i][key]) {
+  //     str += `${key}=${arr[i][key]}`
+  //   }
 
 
-  }
+  // }
 
   // 요청 URL 작성
   const requestUrl = `${mainUrl}/${encodeURIComponent(teamName)}/idols?${str}`
@@ -47,5 +47,53 @@ export const FindIdols19 = async (teamName = '뉴진스', cursor = null, pageSiz
 
   return res
 
+}
+
+export const FindIdolsCharts20 = async (teamName = '뉴진스', cursor = null, pageSize = 10, gender = 'female') => {
+  const arr = [{ cursor }, { pageSize }, { gender }]
+  let str = createQuery(arr);
+
+  console.log("FindIdolsCharts20 str : ", str);
+
+  // 요청 URL 작성
+  const requestUrl = `${mainUrl}/${encodeURIComponent(teamName)}/charts/{gender}?${str}`
+
+  // 서버에 요청
+  const res = await fetch(requestUrl)
+    .then((res) => {
+      console.log("then : ", res)
+      return res.json()
+    })
+    .then((data) => {
+      console.log("data : ", data)
+      return data
+    })
+    .catch(err => console.error(err))
+    .finally(() => console.log("FindIdolsCharts20 완료"))
+
+  console.log(res)
+
+  return res
+}
+
+function createQuery(arr = []) {
+  let str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    const key = Object.keys(arr[i])[0]
+    console.log("check key : ", key);
+
+    // null이 아닌 값은 쿼리로 입력
+    if (arr[i][key]) {
+      str += `${key}=${arr[i][key]}`
+    }
+
+    if (i + 1 < arr.length) {
+      str += '&'
+    }
+
+  }
+
+  return str
 }
 
