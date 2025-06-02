5b9cb34fe07ae727e6c165c8bab78766290f9c90
이관현\Luganic <rhksgus509@gmail.com>
2025-06-02 15:48:23 +0900

    #28 #29 #30 후원을 기다리는 조공 영역 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import HeaderNav from "./component/10_HeaderNav/HeaderNav";
 import CreditBox from "./component_combine/18_CreditBox/CreditBox";
+import BoostIdols from "./component_combine/30_BoostIdols/BoostIdols";
 // import testImage from "./assets/image/test_image2.png"
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <HeaderNav />
       <CreditBox />
+      <BoostIdols />
     </>
   );
 };
