c71c4578f76cc5bad82c1379f03fca10332d294f
yeji <yan0.ye.zi@gmail.com>
2025-06-02 16:45:41 +0900

    마이페이지 기능 구현 및 desktop 디자인 완료 (반응형 구현 전)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import HeaderNav from "./component/10_HeaderNav/HeaderNav";
-import AddFavoriteIdol from "./component_combine/25_AddFavoriteIdol/AddFavoriteIdol";
-import SelectableIdolList from "./component/26_SelectableIdolList/SelectableIdolList.jsx";
+import HeaderNav from "./component/10_HeaderNav/HeaderNav.jsx";
+import FavoriteIdol from "./component/27_FavoriteIdol/FavoriteIdol.jsx";
+import AddFavoriteIdol from "./component_combine/25_AddFavoriteIdol/AddFavoriteIdol.jsx";
 
 const App = () => {
+  const [favoriteIdols, setFavoriteIdols] = useState([]);
+
+  // 처음 마운트 시 localStorage에서 불러오기
+  useEffect(() => {
+    const stored = localStorage.getItem("favoriteIdols");
+    if (stored) {
+      setFavoriteIdols(JSON.parse(stored));
+    }
+  }, []);
+
   return (
     <>
       <HeaderNav />
-      <AddFavoriteIdol />
+      <FavoriteIdol
+        favoriteIdols={favoriteIdols}
+        setFavoriteIdols={setFavoriteIdols}
+      />
+      <hr className="section-divider" />
+      <AddFavoriteIdol
+        favoriteIdols={favoriteIdols}
+        setFavoriteIdols={setFavoriteIdols}
+      />
     </>
   );
 };
