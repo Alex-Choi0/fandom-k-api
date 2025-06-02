ae24e4efbe787e427fea99fab5ab69baac76d71d
alex <alex@gmail.com>
2025-06-02 23:51:42 +0900

    #37 react-router-dom npm 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-
+import { Routes } from "react-router-dom"
+import "./App.css"
 
 const App = () => {
-  const [favoriteIdols, setFavoriteIdols] = useState([]);
+  return <Routes></Routes>
+}
 
-  // 처음 마운트 시 localStorage에서 불러오기
-  useEffect(() => {
-    const stored = localStorage.getItem("favoriteIdols");
-    if (stored) {
-      setFavoriteIdols(JSON.parse(stored));
-    }
-  }, []);
-
-  return (
-    <>
-<h1> 메인페이지</h1>
-    </>
-  );
-};
-
-export default App;
+export default App
