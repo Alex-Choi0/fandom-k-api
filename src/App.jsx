04eda449949e7ec6a3fd66a787ffe02d82b303f9
alex <alex@gmail.com>
2025-06-02 23:55:21 +0900

    #38 랜딩페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import "./App.css"
+import LandingPageRoute38 from "./routes/38_landing_page/38_landing_page.route"
 
 const App = () => {
-  return <Routes></Routes>
+  return (
+    <Routes>
+      <Route index={true} path="/" element={<LandingPageRoute38 />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/routes/38_landing_page/38_landing_page.route.jsx
@@ -0,0 +1,9 @@
+const LandingPageRoute38 = () => {
+  return (
+    <>
+      <h1>랜딩페이지 부분</h1>
+    </>
+  )
+}
+
+export default LandingPageRoute38
