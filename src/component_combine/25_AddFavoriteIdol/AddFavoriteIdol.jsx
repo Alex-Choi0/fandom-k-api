1bcc321c9fa9e75c99a99cc5f0e8217210d55fa4
yeji <yan0.ye.zi@gmail.com>
2025-06-02 14:08:05 +0900

    #25 AddFavoriteIdol UI 완성 (추가하기 기능 구현 전)

--- src/component_combine/25_AddFavoriteIdol/AddFavoriteIdol.jsx
+++ src/component_combine/25_AddFavoriteIdol/AddFavoriteIdol.jsx
@@ -1,12 +1,28 @@
 import "./AddFavoriteIdol.css";
 import Button from "../../component/1_button/1_button.component";
 import iconPlus from "../../assets/icon/plus.svg";
+import SelectableIdolList from "../../component/26_SelectableIdolList/SelectableIdolList.jsx";
+import { useState } from "react";
 
 function AddFavoriteIdol() {
+  const [selectedIds, setSelectedIds] = useState([]); // ✅ 요거 추가
+
+  const handleToggle = (idolId) => {
+    setSelectedIds((prev) =>
+      prev.includes(idolId)
+        ? prev.filter((id) => id !== idolId)
+        : [...prev, idolId]
+    );
+  };
+
   return (
     <div className="AddFavoriteIdol-section">
       <h2 className="AddFavoriteIdol-name">관심 있는 아이돌을 추가해보세요.</h2>
-      <p>아이돌 목록</p>
+
+      {/* ✅ 아이돌 목록 렌더링 */}
+      <SelectableIdolList selectedIds={selectedIds} onToggle={handleToggle} />
+
+      {/* ✅ 버튼은 아래 유지 */}
       <Button
         className="AddFavoriteIdol-button"
         btnName={
