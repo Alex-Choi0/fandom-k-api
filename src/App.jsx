93943c7cdd93edf408bae08411f9eb19de3ee6ec
yeji <yan0.ye.zi@gmail.com>
2025-06-03 20:32:18 +0900

    반응형 작업 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-
+import HeaderNav from "./component/10_HeaderNav/HeaderNav.jsx";
+import FavoriteIdol from "./component/27_favoriteIdol/FavoriteIdol";
+import AddFavoriteIdol from "./component_combine/25_AddFavoriteIdol/AddFavoriteIdol";
 
 const App = () => {
   const [favoriteIdols, setFavoriteIdols] = useState([]);
@@ -15,7 +17,7 @@ const App = () => {
 
   return (
     <>
-<h1> 메인페이지</h1>
+      <h1> 메인페이지</h1>
     </>
   );
 };
