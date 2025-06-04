62993c88ab4206395b52b585250d7d115b034f70
이관현\Luganic <rhksgus509@gmail.com>
2025-06-04 20:35:07 +0900

    후원 목록 리스트 수정

---
+++ src/component_combine/47_ListPage/ListPage.jsx
@@ -0,0 +1,17 @@
+import HeaderNav from "../../component/10_HeaderNav/HeaderNav";
+import CreditBox from "../18_CreditBox/CreditBox";
+import BoostIdols from "../30_BoostIdols/BoostIdols";
+import ChartPage from "../31_ChartPage/ChartPage";
+
+function ListPage() {
+  return (
+    <>
+      <HeaderNav />
+      <CreditBox />
+      <BoostIdols />
+      <ChartPage />
+    </>
+  );
+}
+
+export default ListPage;
