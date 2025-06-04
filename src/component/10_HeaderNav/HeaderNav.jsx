d729afa52d10be3ac960fdf5a3d31e4f2101af44
yeji <yan0.ye.zi@gmail.com>
2025-06-04 22:34:13 +0900

    #10 로고 클릭 시 새로고침, 유저 아이콘 클릭 시 마이페이지 이동 기능 구현

--- src/component/10_HeaderNav/HeaderNav.jsx
+++ src/component/10_HeaderNav/HeaderNav.jsx
@@ -1,15 +1,26 @@
 import "./HeaderNav.css";
 import Logo from "../../assets/image/logo.svg";
 import UserIcon from "../../assets/image/usericon.svg";
+import { useNavigate } from "react-router-dom";
 
 function HeaderNav() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    window.location.reload(); // 새로고침
+  };
+
+  const handleUserIconClick = () => {
+    navigate("/mypage"); // 마이페이지 이동
+  };
+
   return (
     <header className="HeaderNav">
       <div className="HeaderNav-Side" />
-      <div className="HeaderNav-Center">
+      <div className="HeaderNav-Center" onClick={handleLogoClick}>
         <img src={Logo} alt="Fandom-k 로고" />
       </div>
-      <div className="HeaderNav-Right">
+      <div className="HeaderNav-Right" onClick={handleUserIconClick}>
         <img src={UserIcon} alt="유저 아이콘 로고" />
       </div>
     </header>
