18706db0d86eb8bc3068efe3a13267df56426694
yeji <yan0.ye.zi@gmail.com>
2025-06-04 16:17:05 +0900

    마이페이지 라우팅 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Route, Routes } from "react-router-dom"
-import "./App.css"
-import LandingPageRoute38 from "./routes/38_landing_page/38_landing_page.route"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import LandingPageRoute38 from "./routes/38_landing_page/38_landing_page.route";
+import MyPage from "./component_combine/46_MyPage/MyPage";
 
 const App = () => {
   return (
     <Routes>
       <Route index={true} path="/" element={<LandingPageRoute38 />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
