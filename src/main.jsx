071d6d2c57f8a7251670222e1d656d6ad4c67db8
alex <alex@gmail.com>
2025-06-06 12:09:04 +0900

    BrowserRouter basename 수정

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="fandom-k-api">
       <App />
     </BrowserRouter>
   </StrictMode>
