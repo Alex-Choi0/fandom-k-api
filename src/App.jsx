bd0a3a7635196f52220980ff181a440b83142348
이관현\Luganic <rhksgus509@gmail.com>
2025-06-06 18:23:36 +0900

    #62 크레딧 UI 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import LandingPageRoute38 from "./routes/38_landing_page/38_landing_page.route";
 import MyPage from "./component_combine/46_MyPage/MyPage";
 import LandingPageRoute55 from "./routes/55_list_page/55_list_page.route";
+import { CreditProvider } from "./context/52_credit_context/credit_context";
 
 const App = () => {
   return (
-    <Routes>
-      <Route index={true} path="/" element={<LandingPageRoute38 />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route index={true} path="/list" element={<LandingPageRoute55 />} />
-    </Routes>
+    <CreditProvider>
+      <Routes>
+        <Route index={true} path="/" element={<LandingPageRoute38 />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route index={true} path="/list" element={<LandingPageRoute55 />} />
+      </Routes>
+    </CreditProvider>
   );
 };
 
