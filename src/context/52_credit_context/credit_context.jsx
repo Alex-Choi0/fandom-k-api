c83e49daab073ae995270265a6681f6aa3fc69f8
이관현\Luganic <rhksgus509@gmail.com>
2025-06-06 16:36:48 +0900

    #52 크레딧 상태 + local storage 연동 + 함수 적용

--- src/context/52_credit_context/credit_context.jsx
+++ src/context/52_credit_context/credit_context.jsx
@@ -1,16 +1,41 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const CreditContext = createContext(); //context 생성
+const CreditContext = createContext(null); // context 생성, 전역 상태 공간 생성성
 
-function creditprovider({ children }) {
-  const [credit, setCredit] = useState(0);
-
-  useEffect(() => {
-    const storedCredit = localStorage.getItem("credit");
-    if (storedCredit) {
-      setCredit(Number(storedCredit));
-    }
-  }, []);
+/* 커스텀 훅 */
+export function useCreditContext() {
+  return useContext(CreditContext);
 }
 
-export default creditprovider;
+export function CreditProvider({ children }) {
+  const [credit, setCredit] = useState(0); // 크레딧 상태
+
+  /* 크레딧 불러오기 */
+  useEffect(() => {
+    const saved = localStorage.getItem("credit");
+    if (saved !== null) {
+      setCredit(Number(saved));
+    }
+  }, []);
+
+  /* 크레딧 변경 시 localStorage에 저장 */
+  useEffect(() => {
+    localStorage.setItem("credit", credit);
+  }, [credit]);
+
+  /* 크레딧 증가 */
+  const addCredit = (amount) => {
+    setCredit((prev) => prev + amount);
+  };
+
+  /* 크레딧 감소 */
+  const useCredit = (amout) => {
+    setCredit((prev) => Math.max(prev - amout, 0)); //음수일 때 0 처리
+  };
+
+  return (
+    <CreditContext.Provider value={{ credit, addCredit, useCredit }}>
+      {children}
+    </CreditContext.Provider>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fandom-k-ui/',
-})
+  // base: '/fandom-k-ui/',
+  base: "/", // 테스트용
+});
