9f37e6b9fbc2e50358208c68e4157bd7304d5d17
yeji <yan0.ye.zi@gmail.com>
2025-06-07 19:09:51 +0900

    모바일 투표페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPageRoute38 from "./routes/38_landing_page/38_landing_page.route";
 import MyPage from "./component_combine/46_MyPage/MyPage";
 import LandingPageRoute55 from "./routes/55_list_page/55_list_page.route";
 import { CreditProvider } from "./context/52_credit_context/credit_context";
+import ModalVoteMobile from "./modal/71_modal_vote/modal_vote_mobile";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index={true} path="/" element={<LandingPageRoute38 />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route index={true} path="/list" element={<LandingPageRoute55 />} />
+        <Route path="/vote" element={<ModalVoteMobile />} />
       </Routes>
     </CreditProvider>
   );
