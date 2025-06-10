df70e189265bef609a342797db4d75d0be39ce57
alex <alex@gmail.com>
2025-06-10 12:33:22 +0900

    #72 useMemo로 랜더링 횟수 줄이기

--- src/modal/72_recharge_credit/recharge_credit.jsx
+++ src/modal/72_recharge_credit/recharge_credit.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import creditImage from "../../assets/icon/credit.svg"
 import creditBlankImage from "../../assets/svg/credit.svg"
 import ImageComponent2 from "../../component/2_image/2_image.component"
@@ -21,89 +21,93 @@ function RechargeCredit({ onClose, idol, subtitle, title }) {
     console.log("handelRechargeCredit onClose실행")
   }
 
-  return (
-    <>
-      <ModalFrame title="크레딧 충전하기" onClose={onClose}>
-        <div
-          className="single-recharge"
-          style={{ textAlign: "center", marginBottom: "16px" }}
-        >
-          <div class="radio-group">
-            <div
-              className={`recharge-credit set100 ${
-                selected === "100" ? "selected" : ""
-              }`}
-              onClick={() => setSelected("100")}
-            >
-              <div className="credit-count set100">
-                <ImageComponent2 src={creditImage} border="none" />
-                <span>100</span>
-              </div>
-              <input
-                type="radio"
-                id="radio1"
-                name="choice"
-                checked={selected === "100"}
-                onChange={() => setSelected("100")}
-                style={{ display: "none" }}
-              />
-              <label for="radio1"></label>
-            </div>
-            <div
-              className={`recharge-credit set500 ${
-                selected === "500" ? "selected" : ""
-              }`}
-              onClick={() => setSelected("500")}
-            >
-              <div className="credit-count set500">
-                <ImageComponent2 src={creditImage} border="none" />
-                <span>500</span>
-              </div>
-              <input
-                type="radio"
-                id="radio2"
-                name="choice"
-                checked={selected === "500"}
-                onChange={() => setSelected("500")}
-                style={{ display: "none" }}
-              />
-              <label for="radio2"></label>
-            </div>
-            <div
-              className={`recharge-credit set1000 ${
-                selected === "1000" ? "selected" : ""
-              }`}
-              onClick={() => setSelected("1000")}
-            >
-              <div className="credit-count set1000">
-                <ImageComponent2 src={creditImage} border="none" />
-                <span>1000</span>
-              </div>
-              <input
-                type="radio"
-                id="radio3"
-                name="choice"
-                checked={selected === "1000"}
-                onChange={() => setSelected("1000")}
-                style={{ display: "none" }}
-              />
-              <label for="radio3"></label>
-            </div>
-          </div>
-          <button
-            className="chargeCredit-btn"
-            onClick={() => handelRechargeCredit()}
+  const calculatedResult = useMemo(() => {
+    return (
+      <>
+        <ModalFrame title="크레딧 충전하기" onClose={onClose}>
+          <div
+            className="single-recharge"
+            style={{ textAlign: "center", marginBottom: "16px" }}
           >
-            <img
-              src={creditBlankImage}
-              alt="credit"
-              style={{ width: "30px" }}
-            />
-            충전하기
-          </button>
-        </div>
-      </ModalFrame>
-    </>
-  )
+            <div class="radio-group">
+              <div
+                className={`recharge-credit set100 ${
+                  selected === "100" ? "selected" : ""
+                }`}
+                onClick={() => setSelected("100")}
+              >
+                <div className="credit-count set100">
+                  <ImageComponent2 src={creditImage} border="none" />
+                  <span>100</span>
+                </div>
+                <input
+                  type="radio"
+                  id="radio1"
+                  name="choice"
+                  checked={selected === "100"}
+                  onChange={() => setSelected("100")}
+                  style={{ display: "none" }}
+                />
+                <label for="radio1"></label>
+              </div>
+              <div
+                className={`recharge-credit set500 ${
+                  selected === "500" ? "selected" : ""
+                }`}
+                onClick={() => setSelected("500")}
+              >
+                <div className="credit-count set500">
+                  <ImageComponent2 src={creditImage} border="none" />
+                  <span>500</span>
+                </div>
+                <input
+                  type="radio"
+                  id="radio2"
+                  name="choice"
+                  checked={selected === "500"}
+                  onChange={() => setSelected("500")}
+                  style={{ display: "none" }}
+                />
+                <label for="radio2"></label>
+              </div>
+              <div
+                className={`recharge-credit set1000 ${
+                  selected === "1000" ? "selected" : ""
+                }`}
+                onClick={() => setSelected("1000")}
+              >
+                <div className="credit-count set1000">
+                  <ImageComponent2 src={creditImage} border="none" />
+                  <span>1000</span>
+                </div>
+                <input
+                  type="radio"
+                  id="radio3"
+                  name="choice"
+                  checked={selected === "1000"}
+                  onChange={() => setSelected("1000")}
+                  style={{ display: "none" }}
+                />
+                <label for="radio3"></label>
+              </div>
+            </div>
+            <button
+              className="chargeCredit-btn"
+              onClick={() => handelRechargeCredit()}
+            >
+              <img
+                src={creditBlankImage}
+                alt="credit"
+                style={{ width: "30px" }}
+              />
+              충전하기
+            </button>
+          </div>
+        </ModalFrame>
+      </>
+    )
+  }, [selected])
+
+  return calculatedResult
 }
 export default RechargeCredit
